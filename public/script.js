af87a982a1136534995d4c58d58c4ae73147f72f
Olivia Dissen <49534719+OliviaDissen01@users.noreply.github.com>
2021-04-09 07:19:52 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -5,22 +5,62 @@ async function dataHandler() {
 }
 window.onload = getData;
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+async function getMacroById(req, res) {
+  try{
+    const macro = await db.Macros.findAll({
+      where:
+      macro_id: req_params.macro_id
+    });
+    return macro; 
+  }
+  catch (err) {
+    console.error(err):
+    return 'error message';
+  }
+}
+async function getMealById(req, res) {
+  try{
+    const meal = await db.Meals.findAll({
+      where:
+      meal_id: console.log(Math.random())
+    });
+    return meal; 
+  }
+  catch (err) {
+    console.error(err):
+    return 'error message';
+  }
+}
 window.onload = function () {
   const chart = new CanvasJS.Chart('chartContainer', {
     animationEnabled: true,
     title: {
       text: 'Olivia Dissens Chart'
     },
+    legend:{
+      cursor: "pointer",
+      itemclick: toggleDataSeries
+    },
     data: [
       {
         type: 'stackedBar',
         name: 'Meals',
         dataPoints: [
-          { label: 'apple', y: 10 },
-          { label: 'orange', y: 15 },
-          { label: 'banana', y: 25 },
-          { label: 'mango', y: 30 },
-          { label: 'grape', y: 28 }
+          { label: 'meal', y: 'macro'},
+          { label: 'meal', y: 'macro'},
+          { label: 'meal', y: 'macro'},
+          { label: 'meal', y: 'macro'},
+          { label: 'meal', y: 'macro'},
+          { label: 'meal', y: 'macro'},
+          { label: 'meal', y: 'macro'},
+          { label: 'meal', y: 'macro'},
+          { label: 'meal', y: 'macro'},
+          { label: 'meal', y: 'macro'}
+      
         ]
       }
     ]
