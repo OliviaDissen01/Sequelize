2fd19ddb836870d2328bc715fc9382b2754073cb
Olivia Dissen <49534719+OliviaDissen01@users.noreply.github.com>
2021-04-09 06:38:53 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -4,3 +4,26 @@ async function dataHandler() {
   DiningHallList = json;
 }
 window.onload = getData;
+
+window.onload = function () {
+  const chart = new CanvasJS.Chart('chartContainer', {
+    animationEnabled: true,
+    title: {
+      text: 'Olivia Dissens Chart'
+    },
+    data: [
+      {
+        type: 'stackedBar',
+        name: 'Meals',
+        dataPoints: [
+          { label: 'apple', y: 10 },
+          { label: 'orange', y: 15 },
+          { label: 'banana', y: 25 },
+          { label: 'mango', y: 30 },
+          { label: 'grape', y: 28 }
+        ]
+      }
+    ]
+  });
+  chart.render();
+};
